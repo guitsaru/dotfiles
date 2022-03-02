16e0635dadcc72863278ce24124f094d07af5bc6
Matt Pruitt <matt@guitsaru.com>
2022-03-02 21:43:20 +0700

    Format lsp on save

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -87,6 +87,7 @@ return require("packer").startup(function()
   use "hrsh7th/nvim-cmp"
   use "hrsh7th/cmp-vsnip"
   use "hrsh7th/vim-vsnip"
+  use "lukas-reineke/lsp-format.nvim"
 
   use "neovim/nvim-lspconfig"
   use "williamboman/nvim-lsp-installer"
